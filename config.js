c2fca2d019f16ce9b564a42df0a374899f5c992c
Jeff Chapman <jeffcchapman@gmail.com>
2017-03-21 20:32:12 -0600

    finished

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret: 'thisIsaGreatSecretToTryandhack22',
+  appID: 1289710001076500,
+  appSecret: '3df9a6727639c503d12eaf8c8c749758'
+}
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook');
+const bodyParser = require('body-parser');
+const config = require('./config')
+const port = 8000;
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(session({
+  secret: config.secret,
+  saveUninitialized: false,
+  resave: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use('facebook', new FacebookStrategy({
+  clientID: config.appID,
+  clientSecret: config.appSecret,
+  callbackURL: 'http://localhost:8000/auth/facebook/callback',
+  profileFields: ['id', 'displayName']
+},
+  function(token, refreshToken, profile, done){
+    return done(null, profile);
+}));
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', passport.authenticate('facebook',
+  { successRedirect:'/me',
+    failureRedirect: '/login'}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+app.get('/me', function(req, res) {
+  console.log(req.user);
+  res.send(req.user);
+})
+
+app.get('/auth/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+})
+
+app.listen(port, function() {
+  console.log(`app listening on port ${port}`);
+})
