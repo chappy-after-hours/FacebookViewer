a13bff34ceb26c2fb8401e07a7c5504604cedb3d
Jeff Chapman <jeffcchapman@gmail.com>
2017-03-21 20:35:04 -0600

    updated config

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook');
 const bodyParser = require('body-parser');
-const config = require('./config')
+const config = require('./configIt')
 const port = 8000;
 
 
